360569ab9ebd37abfb5c53d83061068043eb6fb0
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-10-11 20:44:35 +0530

    rechart add

--- src/Components/Cheart.js
+++ src/Components/Cheart.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useContext } from "react";
+import {
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Cheart = () => {
-    return (
-        <div>
-            rechat comp
-        </div>
-    );
+  const data = [
+    {
+      id: 1,
+      name: "React",
+      logo: "https://live.staticflickr.com/65535/52413593240_e00326e727_o.png",
+      total: 8,
+    },
+    {
+      id: 2,
+      name: "JavaScript",
+      logo: "https://live.staticflickr.com/65535/52412638962_12e932256a_o.png",
+      total: 9,
+    },
+    {
+      id: 4,
+      name: "CSS",
+      logo: "https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png",
+      total: 8,
+    },
+    {
+      id: 5,
+      name: "Git",
+      logo: "https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png",
+      total: 11,
+    },
+  ];
+
+  return (
+    <div className="mx-auto w-1/2 my-28 py-10">
+      <ComposedChart width={500} height={250} data={data}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Bar dataKey="total" barSize={20} fill="#413ea0" />
+      </ComposedChart>
+    </div>
+  );
 };
 
 export default Cheart;
